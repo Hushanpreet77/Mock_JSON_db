0a548f4220a0fb1ca031c42f461e217c1e5e9575
Hushanpreet77 <hushanpreetkaur86@gmail.com>
2025-04-01 10:37:17 -0600

    update

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express');
-    require('dotenv').config();
-    const PORT = process.env.PORT;
-    const userRoutes = require('./routes/users.js');
-    const app = express();
-    const ratelimit = require(`express-rate-limit`)
+require('dotenv').config();
+const PORT = process.env.PORT;
+const userRoutes = require('./routes/users.js');
+const app = express();
+const ratelimit = require('express-rate-limit');
 
-    //setup our view engine
-    app.set(`view engine`,`ejs`);
-    app.set(`views`, "./views");
+//setup our view engine
+app.set('view engine', 'ejs');
+app.set('views', "./views");
 
-   app.use(express.static('public'));
-   app.use('userRoutes',);
+//rate limiter
+const fixedWindowLimiter = rateLimit({
+    windowMs: 15 * 1000,
+    max: 10,
+    message: 'Too many requests. Please try again later',
+});
 
-  
-    app.listen(PORT, () =>{
-        console.log(`connected to port: ${PORT}`);
-    });
+app.use(fixedWindowLimiter);
+
+app.use(express.static('public'));
+app.use(userRoutes);
